51a2ccf073df714c8b18b17d3d1339de3e302290
Pete Kubiak <peter.kubiak@dyson.com>
2025-02-11 12:01:47 +0000

    Implement simple init

--- src/lib.rs
+++ src/lib.rs
@@ -19,27 +19,26 @@ impl<SPI> Paa5100je<SPI>
 where
     SPI: SpiDevice,
 {
-    pub fn new<BUS: SpiBus, CS: OutputPin>(
-        bus: BUS,
-        mut cs_pin: CS,
-        delay_source: &mut impl DelayNs,
-    ) -> Result<Self, Error<CS::Error>> {
-        cs_pin.set_low()?;
-        delay_source.delay_ms(50);
-        cs_pin.set_high()?;
+    pub fn new(spi: SPI, delay_source: &mut impl DelayNs) -> Result<Self, SPI::Error> {
+        let mut instance = Self { spi };
 
-        let spi = SPI::new(bus, cs_pin);
-        todo!()
+        instance.write(register::POWER_UP_RESET, 0x5A)?;
+        delay_source.delay_ms(20);
+
+        Ok(instance)
     }
 }
 
-pub enum Error<GPIOE> {
-    Gpio(GPIOE),
-}
+impl<SPI> Paa5100je<SPI>
+where
+    SPI: SpiDevice,
+{
+    fn write(&mut self, register: u8, value: u8) -> Result<(), SPI::Error> {
+        self.spi.write(&[register, value])
+    }
 
-impl<GPIOE> From<GPIOE> for Error<GPIOE> {
-    fn from(value: GPIOE) -> Self {
-        Self::Gpio(value)
+    fn read(&mut self, register: u8, buffer: &mut [u8]) -> Result<(), SPI::Error> {
+        self.spi.transfer(buffer, &[register])
     }
 }
 
@@ -47,31 +46,31 @@ trait PixArtSensor {}
 
 impl<SPI> PixArtSensor for Paa5100je<SPI> where SPI: SpiDevice {}
 
-enum Register {
-    ProductId = 0x00,
-    RevisionId = 0x01,
-    Motion = 0x02,
-    DeltaXL = 0x03,
-    DeltaXH = 0x04,
-    DeltaYL = 0x05,
-    DeltaYH = 0x06,
-    Squal = 0x07,
-    RawDataSum = 0x08,
-    MaximumRawData = 0x09,
-    MinimumRawData = 0x0A,
-    ShutterLower = 0x0B,
-    ShutterUpper = 0x0C,
-    Observation = 0x15,
-    MotionBurst = 0x16,
+mod register {
+    pub const PRODUCT_ID: u8 = 0x00;
+    pub const REVISION_ID: u8 = 0x01;
+    pub const MOTION: u8 = 0x02;
+    pub const DELTAX_L: u8 = 0x03;
+    pub const DELTAX_H: u8 = 0x04;
+    pub const DELTAY_L: u8 = 0x05;
+    pub const DELTAY_H: u8 = 0x06;
+    pub const SQUAL: u8 = 0x07;
+    pub const RAW_DATA_SUM: u8 = 0x08;
+    pub const MAXIMUM_RAW_DATA: u8 = 0x09;
+    pub const MINIMUM_RAW_DATA: u8 = 0x0A;
+    pub const SHUTTER_LOWER: u8 = 0x0B;
+    pub const SHUTTER_UPPER: u8 = 0x0C;
+    pub const OBSERVATION: u8 = 0x15;
+    pub const MOTION_BURST: u8 = 0x16;
 
-    PowerUpReset = 0x3A,
-    Shutdown = 0x3B,
+    pub const POWER_UP_RESET: u8 = 0x3A;
+    pub const SHUTDOWN: u8 = 0x3B;
 
-    RawDataGrab = 0x58,
-    RawDataGrabStatus = 0x59,
+    pub const RAW_DATA_GRAB: u8 = 0x58;
+    pub const RAW_DATA_GRAB_STATUS: u8 = 0x59;
 
-    Orientation = 0x5B,
-    InverseProductId = 0x5F,
+    pub const ORIENTATION: u8 = 0x5B;
+    pub const INVERSE_PRODUCT_ID: u8 = 0x5F;
 }
 
 pub fn add(left: u64, right: u64) -> u64 {
